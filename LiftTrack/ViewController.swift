67086884d8b793601cd676702870ec5f6fce0dc8
joncomo <jon@underground.net>
2019-05-23 00:12:58 -0700

    Initial Commit

---
+++ LiftTrack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LiftTrack
+//
+//  Created by Jon Como on 5/23/19.
+//  Copyright © 2019 Jon Como. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
